6349d0fd23179cc8ff7d597bd75c4bce15a204bb
kacper <ks1591@bristol.ac.uk>
2014-03-03 16:46:22 +0000

    conveersion in progres 3 err

--- modmul.c
+++ modmul.c
@@ -81,7 +81,7 @@ int readTuple ( const int n, mpz_t *reader ) {
 }
 
 // define lookup table for hex->bin
-const *char const hexBin = { "0000", "0001", "0010", "0011", "0100", "0101", "0110",
+const char  *const hexBin = { "0000", "0001", "0010", "0011", "0100", "0101", "0110",
   "0111", "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111" } ;
 
 // read in hex representation as binary into an array
